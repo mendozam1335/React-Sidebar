77fa0259462f619f916c0eeb2836aab9ab868c75
mendozam1335 <55033050+mendozam1335@users.noreply.github.com>
2023-10-26 10:26:55 -0500

    Added CSS to project

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "./App";
 
 const Modal = () => {
-  return <div>Modal</div>;
+  const { isModalOpen, closeModal } = useGlobalContext();
+
+  return (
+    <div className={isModalOpen ? "overlay show-modal" : "overlay"}>
+      <div className="modal-content">
+        <button className="modal-close" onClick={closeModal}>
+          <FaTimes />
+        </button>
+        <h2>Modal Content</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
